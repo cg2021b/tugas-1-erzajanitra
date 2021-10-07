42c31133b3cf99d27f00b211dafecca22b0502cc
erzajanitra <75319371+erzajanitra@users.noreply.github.com>
2021-10-07 23:57:54 +0700

    revisi gambar kiri

--- main.js
+++ main.js
@@ -1,134 +1,226 @@
+
+
 function main(){
     /** @type {HTMLCanvasElement} */
     var canvas = document.getElementById('myCanvas');
     /** @type {WebGLRenderingContext} */
    var gl = canvas.getContext('webgl');
-   //curve
-//    const radian= Math.PI / 180;
-   var vertices = []; //tampak atas
+  //tampak atas
+   var vertices = []; 
    var vertCount = 5;
    // kecepatan untuk benda sebelah kanan
    let change = 0;
    let speed = 0.0153;
-   //vertices tampak atas
-   for(var i = 0; i<=90; i+=1){ //rounded corner kiri atas
-       var j = (i + 270) * Math.PI / 180; //atas
-       var k = (i+271) * Math.PI / 180; // bawah
-       var vert1 = [
-           Math.sin(j) * 0.2 -0.7 , Math.cos(j) * 0.2 + 0.2, 0.530, 0.154, 0.154,
-       ];
- 
-       var vert2 = [
-           -0.7, 0.2,  0.690, 0.0690, 0.0690,
-       ];
- 
-       var vert3 = [
-           Math.sin(k) * 0.2 -0.7, Math.cos(k) * 0.2 + 0.2, 0.530, 0.25, 0.154,
-       ];
-       var vert4=[
-        -0.7, 0.2, 0.530, 0.154, 0.154, //kiri bawah
-         -0.7, Math.cos(k) * 0.2 + 0.2, 0.530, 0.154, 0.154, //kiri atas
-         -0.4, 0.2, 0.530, 0.154, 0.154, //kanan atas
-         -0.4, 0.2, 0.530, 0.154, 0.154,
-         -0.7, Math.cos(k) * 0.2 + 0.2, 0.530, 0.154, 0.154,
-         -0.37, Math.cos(k) * 0.2 + 0.2, 0.530, 0.25, 0.154,
-       ];
-       var vert5=[
-        // Math.sin(j) * 0.2 -0.7, -0.3, 0.690, 0.0690, 0.0690, //kiri bawah
-        // Math.sin(j) * 0.2 -0.7, -0.2, 0.690, 0.0690, 0.0690, //kiri atas
-        // -0.4, -0.3, 0.690, 0.0690, 0.0690, //kanan bawah
-        // -0.4, -0.3, 0.690, 0.0690, 0.0690, 
-        Math.sin(j) * 0.2 -0.7, -0.2,0.530, 0.25, 0.154,
-        -0.4, -0.2,0.530, 0.25, 0.154,
-  
-      ];
- 
-       vertices = vertices.concat(vert1);
-       vertices = vertices.concat(vert2);
-       vertices = vertices.concat(vert3);
-       vertices = vertices.concat(vert4);
-       vertices = vertices.concat(vert5);
-    }
-    //rounded corner bawah
-      for(var i = 90; i<=180; i+=1){
-       var j = (i + 90) * Math.PI / 180;
-       var k = (i+91) * Math.PI / 180;
-       var vert1 = [
-           Math.sin(j) * 0.2 - 0.7 , Math.cos(j) * 0.2 -0.2, 0.530, 0.25, 0.154,
-       ];
- 
-       var vert2 = [
-         - 0.7, -0.2, 0.530, 0.25, 0.154,
-       ];
- 
-       var vert3 = [
-           Math.sin(k) * 0.2  - 0.7 , Math.cos(k) * 0.2 -0.2,  0.690, 0.0690, 0.0690,
-       ];
-       var vert4=[
-        - 0.7, -0.4, 0.530, 0.25, 0.154, //kiri bawah
-         -0.4, -0.4, 0.530, 0.25, 0.154, //kiri atas
-         -0.4, Math.cos(j) * 0.2 ,0.530, 0.25, 0.154, //kanan atas
-        //  -0.5, -0.1, 0.690, 0.0690, 0.0690,
-        //  -0.1,Math.cos(j) * 0.2 -0.1, 0.690, 0.0690, 0.0690,
-        //  -0.1, Math.cos(k) * 0.2 + 0.4, 0.690, 0.0690, 0.0690, 
-        ];
- 
-       vertices = vertices.concat(vert1);
-       vertices = vertices.concat(vert2);
-    //    vertices = vertices.concat(vert3);
-       vertices = vertices.concat(vert4);
-   }
-   //rounded corner kanan atas
-   for(var i = 180; i<=270; i+=1){
-        var j = (i + 180) * Math.PI / 180; //atas
-        var k = (i+181) * Math.PI / 180; // bawah
-    var vert1 = [
-        Math.sin(j) * 0.2 - 0.4 , Math.cos(j) * 0.2 +0.2,0.530, 0.25, 0.154,
+      //outer
+    var segitigaTengah1=[
+        -0.815,-0.195,0.820, 0.230, 0.230,
+        -0.815,0.185,0.820, 0.230, 0.230,
+        -0.24,-0.195,0.820, 0.230, 0.230,
     ];
-
-    var vert2 = [
-      - 0.4, 0.2, 0.530, 0.25, 0.154,
+    var segitigaTengah2=[
+        -0.24,0.185,0.820, 0.230, 0.230,
+        -0.815,0.185,0.820, 0.230, 0.230,
+        -0.24,-0.195,0.820, 0.230, 0.230,
     ];
-
-    var vert3 = [
-        Math.sin(k) * 0.2  - 0.4 , Math.cos(k) * 0.2 +0.2, 0.530, 0.25, 0.154,
+    vertices=vertices.concat(segitigaTengah1);
+    vertices=vertices.concat(segitigaTengah2);
+    //bag. corner
+    //kiri atas
+    vertices=vertices.concat(make_bevel(90,-0.815,0.185,0.075,270,0.820, 0.230, 0.230 ));
+    //kiri bawah
+    vertices=vertices.concat(make_bevel(90,-0.815,-0.195,0.075,180,0.820, 0.230, 0.230 ));
+    //kanan atas
+    vertices=vertices.concat(make_bevel(90,-0.24,0.185,0.075,0,0.820, 0.230, 0.230 ));
+    //kanan bawah
+    vertices=vertices.concat(make_bevel(90,-0.24,-0.195,0.075,90,0.820, 0.230, 0.230 ));
+    var segitigaKiri1=[
+        -0.815-0.075,-0.195,0.820, 0.230, 0.230,
+        -0.815,-0.195,0.820, 0.230, 0.230,
+        -0.815-0.075,0.185,0.820, 0.230, 0.230,
     ];
+    var segitigaKiri2=[
+        -0.815,0.185,0.820, 0.230, 0.230,
+        -0.815-0.075,0.185,0.820, 0.230, 0.230,
+        -0.815,-0.195,0.820, 0.230, 0.230,
+    ];
+    var segitigaAtas1=[
+        -0.815,0.185,0.820, 0.230, 0.230,
+        -0.815,0.185+0.075,0.820, 0.230, 0.230,
+        -0.24,0.185,0.820, 0.230, 0.230,
+    ];
+    var segitigaAtas2=[
+        -0.24,0.185+0.075,0.820, 0.230, 0.230,
+        -0.815,0.185+0.075,0.820, 0.230, 0.230,
+        -0.24,0.185,0.820, 0.230, 0.230,
+    ];
+    var segitigaKanan1=[
+        -0.24,-0.195,0.820, 0.230, 0.230,
+        -0.24+0.075,-0.195,0.820, 0.230, 0.230,
+        -0.24,0.185,0.820, 0.230, 0.230,
+    ];
+    var segitigaKanan2=[
+        -0.24+0.075,0.185,0.820, 0.230, 0.230,
+        -0.24+0.075,-0.195,0.820, 0.230, 0.230,
+        -0.24,0.185,0.820, 0.230, 0.230,
+    ];
+    var segitigaBawah1=[
+        -0.815,-0.195-0.075,0.820, 0.230, 0.230,
+        -0.815,-0.195,0.820, 0.230, 0.230,
+        -0.24,-0.195-0.075,0.820, 0.230, 0.230,
+    ];
+    var segitigaBawah2=[
+        -0.24,-0.195,0.820, 0.230, 0.230,
+        -0.815,-0.195,0.820, 0.230, 0.230,
+        -0.24,-0.195-0.075,0.820, 0.230, 0.230,
+    ];
+    vertices=vertices.concat(segitigaKiri1);
+    vertices=vertices.concat(segitigaKiri2);
+    vertices=vertices.concat(segitigaAtas1);
+    vertices=vertices.concat(segitigaAtas2);
+    vertices=vertices.concat(segitigaKanan1);
+    vertices=vertices.concat(segitigaKanan2);
+    vertices=vertices.concat(segitigaBawah1);
+    vertices=vertices.concat(segitigaBawah2);
 
-    vertices = vertices.concat(vert1);
-    vertices = vertices.concat(vert2);
-    vertices = vertices.concat(vert3);
-   
-    }
-    //rounded corner kanan bawah
-    for(var i = 270; i<=360; i+=1){
-        var j = (i + 180) * Math.PI / 180;
-        var k = (i+250) * Math.PI / 180;
-        var vert1 = [
-            Math.sin(j) * 0.2 - 0.4 , Math.cos(j) * 0.2 -0.2, 0.530, 0.25, 0.154,
-        ];
-  
-        var vert2 = [
-          - 0.4, 0.2, 0.530,0.25, 0.154,
-        ];
-  
-        var vert3 = [
-            Math.sin(k) * 0.2  - 0.4 , Math.cos(k) * 0.2 -0.2, 0.530, 0.25, 0.154,
-        ];
-        var vert4=[
-         - 0.7, -0.4,0.530, 0.25, 0.154, //kiri bawah
-          -0.4, -0.4, 0.530, 0.25, 0.154, //kiri atas
-          -0.5, Math.cos(j) * 0.2 -0.4, 0.530, 0.25, 0.154, //kanan atas
-         //  -0.5, -0.1, 0.690, 0.0690, 0.0690,
-         //  -0.1,Math.cos(j) * 0.2 -0.1, 0.690, 0.0690, 0.0690,
-         //  -0.1, Math.cos(k) * 0.2 + 0.4, 0.690, 0.0690, 0.0690, 
-         ];
-  
-        vertices = vertices.concat(vert1);
-        vertices = vertices.concat(vert2);
-        // vertices = vertices.concat(vert3);
-    //     vertices = vertices.concat(vert4);
-    }
-   //vertices2 tampak samping
+    //inner1
+    // var vertices3=[];
+    var inner_ta1=[
+        -0.81,-0.195,0.480, 0.154, 0.154,
+        -0.81,0.185,0.480, 0.154, 0.154,
+        -0.245,-0.195,0.480, 0.154, 0.154
+    ];
+    var inner_ta2=[
+        -0.245,0.185,0.480, 0.154, 0.154,
+        -0.81,0.185,0.480, 0.154, 0.154,
+        -0.245,-0.195,0.480, 0.154, 0.154
+    ];
+    vertices=vertices.concat(inner_ta1);
+    vertices=vertices.concat(inner_ta2);
+    vertices=vertices.concat(make_bevel(90,-0.81,0.185,0.05,270,0.480, 0.154, 0.154 ));
+    //kiri bawah
+    vertices=vertices.concat(make_bevel(90,-0.81,-0.195,0.05,180,0.480, 0.154, 0.154 ));
+    //kanan atas
+    vertices=vertices.concat(make_bevel(90,-0.245,0.185,0.05,0,0.480, 0.154, 0.154 ));
+    //kanan bawah
+    vertices=vertices.concat(make_bevel(90,-0.245,-0.195,0.05,90,0.480, 0.154, 0.154 ));
+    var inner_ta1_atas=[
+        -0.81,0.185,0.480, 0.154, 0.154,
+        -0.81,0.185+0.05,0.480, 0.154, 0.154,
+        -0.245,0.185,0.480, 0.154, 0.154
+    ];
+    var inner_ta2_atas=[
+        -0.245,0.185+0.05,0.480, 0.154, 0.154,
+        -0.81,0.185+0.05,0.480, 0.154, 0.154,
+        -0.245,0.185,0.480, 0.154, 0.154
+    ];
+    var inner_ta1_kanan=[
+        -0.245,-0.195,0.480, 0.154, 0.154,
+        -0.245,0.185,0.480, 0.154, 0.154,
+        -0.245+0.05,-0.195,0.480, 0.154, 0.154,
+    ];
+    var inner_ta2_kanan=[
+        -0.245+0.05,0.185,0.480, 0.154, 0.154,
+        -0.245,0.185,0.480, 0.154, 0.154,
+        -0.245+0.05,-0.195,0.480, 0.154, 0.154,
+    ];
+    var inner_ta1_bwh=[
+        -0.81,-0.195-0.05,0.480, 0.154, 0.154,
+        -0.81,-0.195,0.480, 0.154, 0.154,
+        -0.245,-0.195-0.05,0.480, 0.154, 0.154
+    ];
+    var inner_ta2_bwh=[
+        -0.245,-0.195,0.480, 0.154, 0.154,
+        -0.81,-0.195,0.480, 0.154, 0.154,
+        -0.245,-0.195-0.05,0.480, 0.154, 0.154
+    ];
+    var inner_ta1_ki=[
+        -0.81-0.05,-0.195,0.480, 0.154, 0.154,
+        -0.81-0.05,0.185,0.480, 0.154, 0.154,
+        -0.81,-0.195,0.480, 0.154, 0.154,
+    ];
+    var inner_ta2_ki=[
+        -0.81,0.185,0.480, 0.154, 0.154,
+        -0.81-0.05,0.185,0.480, 0.154, 0.154,
+        -0.81,-0.195,0.480, 0.154, 0.154,
+    ];
+    vertices=vertices.concat(inner_ta1_atas);
+    vertices=vertices.concat(inner_ta2_atas);
+    vertices=vertices.concat(inner_ta1_kanan);
+    vertices=vertices.concat(inner_ta2_kanan);
+    vertices=vertices.concat(inner_ta1_bwh);
+    vertices=vertices.concat(inner_ta2_bwh);
+    vertices=vertices.concat(inner_ta1_ki);
+    vertices=vertices.concat(inner_ta2_ki);
+    //core
+    var core_ta1=[
+        -0.785,-0.175,0.410, 0.139, 0.139,
+        -0.785,0.165,0.410, 0.139, 0.139,
+        -0.265,-0.175,0.410, 0.139, 0.139
+    ];
+    var core_ta2=[
+        -0.265,0.165,0.410, 0.139, 0.139,
+        -0.785,0.165,0.410, 0.139, 0.139,
+        -0.265,-0.175,0.410, 0.139, 0.139
+    ];
+    vertices=vertices.concat(core_ta1);
+    vertices=vertices.concat(core_ta2);
+    vertices=vertices.concat(make_bevel(90,-0.785,0.165,0.035,270,0.410, 0.139, 0.139 ));
+    //kiri bawah
+    vertices=vertices.concat(make_bevel(90,-0.785,-0.175,0.035,180,0.410, 0.139, 0.139));
+    //kanan atas
+    vertices=vertices.concat(make_bevel(90,-0.265,0.165,0.035,0.410, 0.139, 0.139));
+    //kanan bawah
+    vertices=vertices.concat(make_bevel(90, -0.265,-0.175,0.035,90,0.410, 0.139, 0.139 ));
+    var core_ta1_atas=[
+        -0.785,0.165,0.410, 0.139, 0.139,
+        -0.785,0.165+0.035,0.410, 0.139, 0.139,
+        -0.265,0.165,0.410, 0.139, 0.139
+    ];
+    var core_ta2_atas=[
+        -0.265,0.165+0.035,0.410, 0.139, 0.139,
+        -0.785,0.165+0.035,0.410, 0.139, 0.139,
+        -0.265,0.165,0.410, 0.139, 0.139
+    ];
+    var core_ta1_ka=[
+        -0.265,-0.175,0.410, 0.139, 0.139,
+        -0.265,0.165,0.410, 0.139, 0.139,
+        -0.265+0.035,-0.175,0.410, 0.139, 0.139
+    ];
+    var core_ta2_ka=[
+        -0.265+0.035,0.165,0.410, 0.139, 0.139,
+        -0.265,0.165,0.410, 0.139, 0.139,
+        -0.265+0.035,-0.175,0.410, 0.139, 0.139
+    ];
+    var core_ta1_bwh=[
+        -0.785,-0.175-0.035,0.410, 0.139, 0.139,
+        -0.785,-0.175,0.410, 0.139, 0.139,
+        -0.265,-0.175-0.035,0.410, 0.139, 0.139
+    ];
+    var core_ta2_bwh=[
+        -0.265,-0.175,0.410, 0.139, 0.139,
+        -0.785,-0.175,0.410, 0.139, 0.139,
+        -0.265,-0.175-0.035,0.410, 0.139, 0.139
+    ];
+    var core_ta1_ki=[
+        -0.785-0.035,-0.175,0.410, 0.139, 0.139,
+        -0.785-0.035,0.165,0.410, 0.139, 0.139,
+        -0.785,-0.175,0.410, 0.139, 0.139,
+    ];
+    var core_ta2_ki=[
+        -0.265,0.165,0.410, 0.139, 0.139,
+        -0.785-0.035,0.165,0.410, 0.139, 0.139,
+        -0.785,-0.175,0.410, 0.139, 0.139,
+    ];
+    vertices=vertices.concat(core_ta1_atas);
+    vertices=vertices.concat(core_ta2_atas);
+    vertices=vertices.concat(core_ta1_ka);
+    vertices=vertices.concat(core_ta2_ka);
+    vertices=vertices.concat(core_ta1_bwh);
+    vertices=vertices.concat(core_ta2_bwh);
+    vertices=vertices.concat(core_ta1_ki);
+    vertices=vertices.concat(core_ta2_ki);
+
+    //vertices2 tampak samping
     var vertices2=[];
     // color code : 0.690, 0.0690, 0.0690
     var vert1=[
@@ -149,6 +241,7 @@ function main(){
     vertices2= vertices2.concat(vert2);
 
    var n = vertices.length / vertCount;
+
    var n2 = vertices2.length / vertCount;
    var ver_comb = [...vertices, ...vertices2]
       
@@ -226,7 +319,7 @@ function main(){
        // Draw a line
        const u_matrix = gl.getUniformLocation(shaderProgram, 'u_matrix');
        gl.uniformMatrix4fv(u_matrix, false, object1);
-       gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);       
+       gl.drawArrays(gl.TRIANGLES, 0, n);       
        gl.uniformMatrix4fv(u_matrix, false, object2);
        gl.drawArrays(gl.TRIANGLE_STRIP, n, n2);
    
@@ -237,4 +330,30 @@ function main(){
   
    
 
+}
+//membuat lingkaran
+function make_bevel(degree, px = 0, py = 0 , radius = 1, start_degree = 0, r, g, b){
+    let points = []
+
+    for(let i = 0; i <= degree; i++){
+        let j = (i + start_degree) * Math.PI / 180;
+        let k = (i + start_degree + 1) * Math.PI / 180;
+    
+        let vert1 = [
+            Math.sin(j) * radius + px, Math.cos(j) * radius + py, r, g, b
+        ]
+    
+        let vert2 = [
+            px, py, r, g, b
+        ]
+        let vert3 = [
+            Math.sin(k) * radius + px, Math.cos(k) * radius + py, r, g, b
+        ]
+    
+        points = points.concat(vert1);
+        points = points.concat(vert2);
+        points = points.concat(vert3);
+    }
+
+    return points;
 }
